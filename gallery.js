9f40e3db67d06aa1d92fa727291d94ba01d9ab16
hitanshchadha <118474002+hitanshchadha@users.noreply.github.com>
2023-03-07 13:20:58 +0530

    Added carousel indicators

--- gallery.js
+++ gallery.js
@@ -3,11 +3,27 @@ const leftarrow= document.querySelector('.Arrow-left');
 const rightarrow= document.querySelector('.Arrow-right');
 
 var index=0;
+
+
+
 leftarrow.addEventListener('click',function(){
     index=(index>0)? index-1 :0;
-slider.style.transform= 'translate('+ (index) * -100 + '%)';
-});
+    document.querySelector('.controls .selected').classList.remove('.selected');
+    document.querySelector('.controls ul').children[index].classList.add('.selected');
+    slider.style.transform= 'translate('+ (index) * -100 + '%)';
+     });
 rightarrow.addEventListener('click',function(){
     index=(index<3)? index+1: 3;
+    document.querySelector('.controls .selected').classList.remove('.selected');
+    document.querySelector('.controls ul').children[index].classList.add('.selected');
     slider.style.transform= 'translate('+ (index) * -100 + '%)';
     });
+
+    document.querySelectorAll('.controls li').forEach(function(indicator,ind){
+        indicator.addEventListener('click', function(){
+        index=ind;
+        document.querySelector('.controls .selected').classList.remove('.selected');
+        indicator.classList.add('.selected');
+        slider.style.transform= 'translate('+ (index) * -100 + '%)';
+        });
+    });    
