c4bfe4012fdb0b122349029ac9a227741ea9b5fa
Himanshu Kumar <kumarh664@gmail.com>
2023-04-23 14:46:00 +0530

    updated landing page and added colors

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-particlesJS.load('particles-js', 'particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+particlesJS.load('sigs', 'particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+var typed = new Typed('#test', {
+  strings: ['IEEE BITS Pilani Student Chapter', 'innovate.<br> achieve.<br> lead.', 'IEEE BITS Pilani Student Chapter'],
+  typeSpeed: 50,
+  showCursor: false,
+});
+
+var typed = new Typed('#test1', {
+  strings: ['welcome to'],
+  typeSpeed: 50,
+  showCursor: false,
+});
